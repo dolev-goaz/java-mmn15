75598e18ee5f04ce73c42b5849fa04741e4f38e5
dolev-goaz <www.dolev@gmail.com>
2024-04-18 15:26:45 +0300

    begin airport

---
+++ src/q2/Airport.java
@@ -0,0 +1,34 @@
+package q2;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Airport {
+
+    private final String name;
+    private final Lock lock;
+    private boolean[] runwayTakenStatus;
+
+    public Airport(String name, int runwayCount) {
+        this.name = name;
+        this.runwayTakenStatus = new boolean[runwayCount];
+        for (int i = 0; i < runwayCount; i++) {
+            this.runwayTakenStatus[i] = false;
+        }
+        lock = new ReentrantLock();
+    }
+
+    public int depart(int flightNo) {
+        return -1;
+    }
+
+    public int land(int flightNo) {
+        locks[0].
+        return -1;
+    }
+
+    public void freeRunway(int flightNo, int runwayNo) {
+
+    }
+
+}
