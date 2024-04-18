d9fd11d7ccad1369ff705b3c232cc8747210ac6b
dolev goaz <www.dolev@gmail.com>
2024-04-18 18:42:14 +0300

    basic airport logic(no ordering)

--- src/q2/Airport.java
+++ src/q2/Airport.java
@@ -1,5 +1,7 @@
 package q2;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -7,27 +9,43 @@ public class Airport {
 
     private final String name;
     private final Lock lock;
-    private boolean[] runwayTakenStatus;
+    List<Integer> availableRunways;
 
     public Airport(String name, int runwayCount) {
         this.name = name;
-        this.runwayTakenStatus = new boolean[runwayCount];
+        this.availableRunways = new ArrayList<Integer>();
         for (int i = 0; i < runwayCount; i++) {
-            this.runwayTakenStatus[i] = false;
+            this.availableRunways.add(i);
         }
-        lock = new ReentrantLock();
+        this.lock = new ReentrantLock();
     }
 
     public int depart(int flightNo) {
-        return -1;
+        int runway = occupyRunway(flightNo);
+        System.out.println(String.format("Flight #%d is departing from runway %d in airport %s", flightNo, runway, name));
+        return runway;
     }
 
     public int land(int flightNo) {
-        return -1;
+        int runway = occupyRunway(flightNo);
+        System.out.println(String.format("Flight #%d is landing in runway %d in airport %s", flightNo, runway, name));
+        return runway;
     }
 
-    public void freeRunway(int flightNo, int runwayNo) {
+    public synchronized void freeRunway(int flightNo, int runwayNo) {
+        availableRunways.add(runwayNo);
+        notify(); // its enough to notify a single flight since only one runway is freed
+    }
 
+    private synchronized int occupyRunway(int flightNo) {
+        while (availableRunways.size() == 0) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return availableRunways.remove(availableRunways.size() - 1); // remove from the end
     }
 
 }
