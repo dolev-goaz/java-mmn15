a862c27dfc3a1ad95b745556832c3955a5b190dc
dolev goaz <www.dolev@gmail.com>
2024-05-25 15:45:43 +0300

    cleanup, comments

--- src/q2/Flight.java
+++ src/q2/Flight.java
@@ -2,10 +2,15 @@ package q2;
 
 import java.util.Random;
 
+// Class representing a flight, extends thread
 public class Flight extends Thread {
+    private static final int SEC_TO_MS = 1000;
+    private static final int MIN_RANDOM_TIME = 2;
+    private static final int MAX_RANDOM_TIME = 5;
     private final Airport source, target;
     private final int flightNo;
 
+    // constructor
     public Flight(int flightNo, Airport source, Airport target) {
         this.source = source;
         this.target = target;
@@ -13,6 +18,7 @@ public class Flight extends Thread {
     }
 
     @Override
+    // running the thread(flight)
     public void run() {
         super.run();
 
@@ -21,41 +27,50 @@ public class Flight extends Thread {
         land();
     }
 
+    // flight take off
     private void takeoff() {
         int runway = source.depart(flightNo);
         try {
-            Thread.sleep(randomDurationTakeoffLandingMS());
-        } catch (InterruptedException ignored) {
+            waitRandomTime();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         } finally {
             source.freeRunway(flightNo, runway);
         }
     }
 
+    // flight in-air
     private void fly() {
         try {
-            Thread.sleep(randomDurationFlightMS());
+            waitRandomTime();
         } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
+    // flight landing
     private void land() {
         int runway = target.land(flightNo);
         try {
-            Thread.sleep(randomDurationTakeoffLandingMS());
-        } catch (InterruptedException ignored) {
+            waitRandomTime();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         } finally {
             target.freeRunway(flightNo, runway);
         }
     }
 
-    private int randomDurationFlightMS() {
-        return random(2, 5) * 1000;
+    // waits for a random amount of time
+    private void waitRandomTime() throws InterruptedException {
+        Thread.sleep(generateRandomTimeMS());
     }
 
-    private int randomDurationTakeoffLandingMS() {
-        return random(2, 5) * 1000;
+    // generates a random amount of time in milliseconds
+    private int generateRandomTimeMS() {
+        return random(MIN_RANDOM_TIME, MAX_RANDOM_TIME) * SEC_TO_MS;
     }
 
+    // returns a random number between min(inclusive) and max(exclusive)
     private int random(int min, int max) {
         return new Random().nextInt(max-min) + min;
     }
--- src/q2/Main.java
+++ src/q2/Main.java
@@ -7,6 +7,8 @@ public class Main {
     private static final int SECOND_RUNWAY_COUNT = 3;
     private static final int FLIGHT_COUNT = 10;
 
+    private static final int FLIGHT_NUMBER_OFFSET = 500;
+
     public static void main(String[] args) {
         Airport first = new Airport("Israel", FIRST_RUNWAY_COUNT);
         Airport second = new Airport("Greece", SECOND_RUNWAY_COUNT);
@@ -14,6 +16,7 @@ public class Main {
         runFlights(flights);
     }
 
+    // begins the provided flights
     private static void runFlights(Flight[] flights) {
         for (Flight flight : flights) {
             flight.start();
@@ -23,17 +26,20 @@ public class Main {
                 flight.join();
             }
         } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
+    // generates an array of flights
     private static Flight[] generateFlights(Airport first, Airport second) {
         Flight[] flights = new Flight[FLIGHT_COUNT];
         for (int i = 0; i < FLIGHT_COUNT; i++) {
-            flights[i] = createRandomFlight(500 + i, first, second);
+            flights[i] = createRandomFlight(FLIGHT_NUMBER_OFFSET + i, first, second);
         }
         return flights;
     }
 
+    // creates a flight between the first and second airport(random direction)
     private static Flight createRandomFlight(int flightNo, Airport first, Airport second) {
         boolean isDepartFromFirst = new Random().nextBoolean();
 
