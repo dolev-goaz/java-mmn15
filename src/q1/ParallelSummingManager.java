3fe54f1a7a38d25cdb92cdee771aafd0c9ebb001
dolev goaz <www.dolev@gmail.com>
2024-06-20 20:48:58 +0300

    comments, actually use fixed executorService

--- src/q1/ParallelSummingManager.java
+++ src/q1/ParallelSummingManager.java
@@ -10,16 +10,14 @@ import java.util.concurrent.TimeUnit;
 // class representing a manager for parallel summing
 public class ParallelSummingManager {
 
-    // amount returned for each summation
-    private static final int BATCH_COUNT = 2;
+    // batch size for each summation
+    private static final int BATCH_SIZE = 2;
 
-    private final int threadCount;
     private final ExecutorService executor;
     private final List<Integer> pool;
 
     // constructor
     public ParallelSummingManager(Integer[] dataSet, int threadCount) {
-        this.threadCount = threadCount;
         executor = Executors.newFixedThreadPool(threadCount);
         pool = new ArrayList<>();
         Collections.addAll(pool, dataSet);
@@ -27,7 +25,12 @@ public class ParallelSummingManager {
 
     // create all the summation workers to run in parallel
     public int parallelSum() {
-        for (int i = 0; i < threadCount; i++) {
+        // each summation we remove BATCH_SIZE items and add 1 item. so each summation removes (BATCH_SIZE - 1) items.
+        // we finish when there's 1 item left in the pool. so we in total remove (pool.size() - 1) items.
+        // so the total amount of steps is the division of the two, rounded up.
+        final double amountOfAdditions = Math.ceil((pool.size() - 1.0) / (BATCH_SIZE - 1.0));
+
+        for (int i = 0; i < amountOfAdditions; i++) {
             executor.execute(new SummationWorker(this));
         }
         executor.shutdown();
@@ -46,11 +49,17 @@ public class ParallelSummingManager {
             // nothing to sum- only the result(at most) remains in the pool
             throw new IndexOutOfBoundsException();
         }
-        int count = Math.min(BATCH_COUNT, pool.size());
+
+        // clamp batch size so if there aren't enough items, return the existing amount
+        int count = Math.min(BATCH_SIZE, pool.size());
+
+        // build the returned items array
         int[] out = new int[count];
         for (int i = 0; i < count; i++) {
             out[i] = pool.remove(pool.size() - 1);
         }
+
+        // return the result
         return out;
     }
 
